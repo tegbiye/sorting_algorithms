feb28f0d6cdc3340a4366b08d6c72db2f82221f4
tegbiye <se.tegbabu.solomon@gmail.com>
2022-10-11 17:29:30 +0300

    Fixed error in task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -26,20 +26,30 @@ aux1 = head_tmp2;
 aux2 = head_tmp2->next;
 aux1->next = aux2->next;
 if (aux2->next)
+{
 	aux2->next->prev = aux1;
+}
 if (aux2)
+{
 	aux2->prev = aux1->prev;
 	aux2->next = aux1;
+}
 if (aux1)
+{
 	aux1->prev = aux2;
+}
 if (aux2->prev)
+{
 	aux2->prev->next = aux2;
 	head_tmp2 = aux2->prev;
+}
 if (!aux2->prev)
+{
 	*list = aux2;
+}
 print_list(*list);
 flag = 1;
-				}
+}
 }
 if (flag == 0)
 	head_tmp1 = head_tmp1->next;
