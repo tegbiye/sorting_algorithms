9ccbd8d8df72efa9b1938383096ab1f4d3394971
tegbiye <se.tegbabu.solomon@gmail.com>
2022-10-12 17:24:28 +0300

    Fixed error in task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,58 +1,49 @@
 #include "sort.h"
+
 /**
- * insertion_sort_list - sorts a doubly linked list of integers
- * in ascending order using the Insertion sort ailgorithm
- * @list: pointer to the list head
- * Return: no return
- **/
+ *swap_node - swap a node for his previous one
+ *@node: node
+ *@list: node list
+ *Return: return a pointer to a node which was enter it
+ */
+listint_t *swap_node(listint_t *node, listint_t **list)
+{
+	listint_t *back = node->prev, *current = node;
+	/*NULL, 19, 48, 9, 71, 13, NULL*/
+
+	back->next = current->next;
+	if (current->next)
+		current->next->prev = back;
+	current->next = back;
+	current->prev = back->prev;
+	back->prev = current;
+	if (current->prev)
+		current->prev->next = current;
+	else
+		*list = current;
+	return (current);
+}
+
+/**
+ * insertion_sort_list - function that sorts a doubly linked list
+ * of integers in ascending order using the Insertion sort algorithm
+ * @list: Dobule linked list to sort
+ */
+
 void insertion_sort_list(listint_t **list)
 {
-listint_t *head_tmp1, *head_tmp2, *aux1, *aux2;
-int flag;
+	listint_t *node;
 
-if (list)
-{
-head_tmp1 = *list;
-head_tmp2 = *list;
-while (list && head_tmp1->next)
-{
-if (head_tmp1->next)
-{
-flag = 0;
-head_tmp2 = head_tmp1;
-while (head_tmp2 && head_tmp2->n > head_tmp2->next->n)
-{
-aux1 = head_tmp2;
-aux2 = head_tmp2->next;
-aux1->next = aux2->next;
-if (aux2->next)
-{
-	aux2->next->prev = aux1;
-}
-if (aux2)
-{
-	aux2->prev = aux1->prev;
-	aux2->next = aux1;
-}
-if (aux1)
-{
-	aux1->prev = aux2;
-}
-if (aux2->prev)
-{
-	aux2->prev->next = aux2;
-	head_tmp2 = aux2->prev;
-}
-if (!aux2->prev)
-{
-	*list = aux2;
-}
-print_list(*list);
-flag = 1;
-}
-}
-if (flag == 0)
-	head_tmp1 = head_tmp1->next;
-}
-}
+	if (list == NULL || (*list)->next == NULL)
+		return;
+	node = (*list)->next;
+	while (node)
+	{
+		while ((node->prev) && (node->prev->n > node->n))
+		{
+			node = swap_node(node, list);
+			print_list(*list);
+		}
+		node = node->next;
+	}
 }
